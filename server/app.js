d4997a72cc8f9424361983c265a5b6630478c343
sami.rolina <sami.rolina@outlook.fr>
2024-06-14 14:44:22 +0200

    connection de l'api a la abse de donnée mysql

--- server/app.js
+++ server/app.js
@@ -1,15 +1,30 @@
-const express = require("express");
-const mysql = require("mysql");
-const myConnection = require("express-myconnection");
+const express = require('express');
+const mysql = require('mysql2');
+const myConnection = require('express-myconnection');
+require('dotenv').config();
+
 const app = express();
 
-const optionBd = {
-    host: 'localhost'
-}
+const dbOptions = {
+  host: 'localhost',
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  port: '3306',
+};
 
-app.use((req,res) => {
-    res.status(404).render("pageIntrouvables");
-})
+app.use(myConnection(mysql, dbOptions, 'single'));
 
-app.listen(3001);
-console.log("Attente de reqêtes au port 3001");
+app.get('/', (req, res) => {
+  req.getConnection((err, connection) => {
+    if (err) return res.send('Connection error');
+    connection.query('SELECT 1 + 1 AS solution', (err, results) => {
+      if (err) return res.send('Query error');
+      res.send(`The solution is: ${results[0].solution}`);
+    });
+  });
+});
+
+app.listen(3001, () => {
+  console.log('Server is running on port 3001');
+});
